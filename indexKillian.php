2e8bd33b2f555a628a06ce8ffffba10016ddaf10
Killian <fourgeaud.killian@gmail.com>
2017-11-23 08:52:07 +0100

    ajout indexKillian

---
+++ indexKillian.php
@@ -0,0 +1,18 @@
+<?php
+require_once("Include/fct.inc.php");
+require_once("Include/class.pdogsb.inc.php");
+include("Vue/v_entete.php") ; 
+session_start();
+$pdo = PdoGsb::getPdoGsb();
+$estConnecte = estConnecte();
+if(!isset($_REQUEST['uc']) || !$estConnecte){
+     $_REQUEST['uc'] = 'connexion';
+}	 
+$uc = $_REQUEST['uc'];
+switch($uc){
+	case 'connexion':{
+		//include("controleurs/c_connexion.php");break;
+	}
+}
+//include("Vue/v_pied.php") ;
+?>
