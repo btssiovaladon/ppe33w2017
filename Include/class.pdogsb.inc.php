d1baa6cb64305cd97c0a8fefb2fd74969711d7ca
Alexis Charrier <alexis.charrier96@gmail.com>
2017-11-23 09:58:06 +0100

    modif pdo creeNouveauDiner

--- Include/class.pdogsb.inc.php
+++ Include/class.pdogsb.inc.php
@@ -175,6 +175,22 @@ class PdoGsb{
 			return $lignes;
 	}
 	
+	/**
+ * Crée un nouveau diner à partir des informations fournies en paramètre
+ 
+ * @param $dateDiner
+ * @param $heure
+ * @param $prix
+ * @param $nbPlace
+ * @param $lieu
+*/
+	public function creeNouveauDiner($dateDiner,$heure,$prix,$nbPlace,$lieu){
+		$dateFr = dateFrancaisVersAnglais($dateDiner);
+		$req = "insert into repas
+		values(NULL,'$heure','$dateDiner','$prix','$nbPlace','$lieu')";
+		PdoGsb::$monPdo->exec($req);
+	}
+	
 }
 
 ?>
