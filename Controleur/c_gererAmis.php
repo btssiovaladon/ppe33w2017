834d4c4def0248372231c18729a64be8bef4bfc6
Guillaume Baudouin <guillaume.baudouin.jd@gmail.com>
2017-11-23 09:19:43 +0100

    c_gererAmis

---
+++ Controleur/c_gererAmis.php
@@ -0,0 +1,11 @@
+<?php
+$amis = $_REQUEST['amis'];
+switch($amis){
+	case 'a_ajouteAmis':{
+		//appel fonction verif
+		//si verif ok appel vue
+		include("Vue/v_creationAmis.php");
+		break;
+	}
+}
+?>
