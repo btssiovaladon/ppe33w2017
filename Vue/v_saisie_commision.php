a3c00846fa3950493c477aa9aada868e64ae3521
victor <victor.botte@laposte.net>
2017-12-14 09:36:15 +0100

    essait

---
+++ Vue/v_saisie_commision.php
@@ -0,0 +1,46 @@
+<?php ?>
+<html>
+		<form action="" method="POST">
+			<fieldset>
+					choix commision <select name="choix_personne">
+						<?php  foreach ($tab as $tab) { ?>
+							<option> <?php echo $tab[$i] ?> </option>
+						<?php } ?>
+
+						</select>	
+					<input type="submit" value="valider" name="envoyer"/>
+			</fieldset>
+		</form>
+		
+
+		</br>
+		</br>
+		</br>
+
+		<?php
+			if (isset($_POST["envoyer"])){
+		?>
+		<fieldset>
+
+			<form>
+				choix fonction <select name="choix_personne">
+					<?php ?>
+					<option> président </option>
+					<option> secretaire </option>
+					<option> coordinateur </option>
+				</select>
+
+				choix amis <select name="choix_personne">
+					<?php ?>
+					<option> liste autocomplétion </option>
+				</select>					
+			</form>
+		</fieldset>	
+		<?php
+		}
+		?>
+
+		
+
+</html>
+
